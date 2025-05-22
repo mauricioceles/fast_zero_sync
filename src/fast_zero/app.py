6ad0180b03c289b41963a4ca9d7bd0c16f1caa60
Mauricio Celes <mauricioceles@gmail.com>
2025-05-21 23:54:17 -0300

    Acréscimo de mais algumas aulas de fastapi

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -2,11 +2,19 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='Minha API BALA')
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,18 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
